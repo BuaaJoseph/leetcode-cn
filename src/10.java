1a237e638c3a95263e750b2e4137cb689892aa60
Joseph <buaa_dcc@163.com>
2020-01-09 10:25:03 +0800

    【困难】正则表达式(regular-expression-matching)
    【困难】正则表达式(regular-expression-matching)

---
+++ src/10.java
@@ -0,0 +1,66 @@
+/**
+ * 给你一个字符串 s 和一个字符规律 p，请你来实现一个支持 '.' 和 '*' 的正则表达式匹配。
+ * 
+ * '.' 匹配任意单个字符 '*' 匹配零个或多个前面的那一个元素 所谓匹配，是要涵盖 整个 字符串 s的，而不是部分字符串。
+ * 
+ * 说明:
+ * 
+ * s 可能为空，且只包含从 a-z 的小写字母。 p 可能为空，且只包含从 a-z 的小写字母，以及字符 . 和 *。 示例 1:
+ * 
+ * 输入: s = "aa" p = "a" 输出: false 解释: "a" 无法匹配 "aa" 整个字符串。 示例 2:
+ * 
+ * 输入: s = "aa" p = "a*" 输出: true 解释: 因为 '*' 代表可以匹配零个或多个前面的那一个元素, 在这里前面的元素就是
+ * 'a'。因此，字符串 "aa" 可被视为 'a' 重复了一次。 示例 3:
+ * 
+ * 输入: s = "ab" p = ".*" 输出: true 解释: ".*" 表示可匹配零个或多个（'*'）任意字符（'.'）。 示例 4:
+ * 
+ * 输入: s = "aab" p = "c*a*b" 输出: true 解释: 因为 '*' 表示零个或多个，这里 'c' 为 0 个, 'a'
+ * 被重复一次。因此可以匹配字符串 "aab"。 示例 5:
+ * 
+ * 输入: s = "mississippi" p = "mis*is*p*." 输出: false
+ * 
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/regular-expression-matching
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+class Solution {
+    public boolean isMatch(String s, String p) {
+        if (s == null || p == null) {
+            return false;
+        }
+        return match(s, 0, p, 0);
+    }
+
+    private boolean match(String s, int s1, String p, int p1) {
+        boolean isStrDone = s1 >= s.length();
+        boolean isPatDone = p1 >= p.length();
+
+        if (isStrDone && isPatDone) {
+            return true;
+        }
+        if (!isStrDone && isPatDone) {
+            return false;
+        }
+        // 一、下一个字符是*
+        if (p1 + 1 < p.length() && p.charAt(p1 + 1) == '*') {
+            if (isStrDone) {
+                return match(s, s1, p, p1 + 2);
+            }
+            if (p.charAt(p1) == s.charAt(s1) || p.charAt(p1) == '.' && s.charAt(s1) != '\0') {
+                return match(s, s1 + 1, p, p1) || match(s, s1, p, p1 + 2);
+            } else {
+                return match(s, s1, p, p1 + 2);
+            }
+        }
+        if (isStrDone && !isPatDone) {
+            return false;
+        }
+
+        // 二、下一个字符不是*
+        if (p.charAt(p1) == s.charAt(s1) || p.charAt(p1) == '.' && s.charAt(s1) != '\0') {
+            return match(s, s1 + 1, p, p1 + 1);
+        }
+        return false;
+
+    }
+}
